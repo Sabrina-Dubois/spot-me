7cc002fe1e991dac513f8209aa62c7506a8d2115
Dubois Sabrina <dubois.sabrina84@gmail.com>
2024-06-24 16:59:02 +0200

    add DDL and DML scripts #1

---
+++ spot-me-database/Scripts/Schema.ddl.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS t_spots;
+
+CREATE TABLE t_spots(
+   id_spot INT GENERATED ALWAYS AS IDENTITY, -- SMALL/BIG
+   spot_name VARCHAR(200),
+   spot_lat DECIMAL(9,6),
+   spot_lng DECIMAL(9,6),
+   spot_desc VARCHAR(2000),
+   spot_img VARCHAR(60),
+   CONSTRAINT t_id_spots_pkey PRIMARY KEY(id_spot),
+   CONSTRAINT t_spots_ukey UNIQUE(spot_name, spot_lat, spot_lng)
+);
+
---
+++ spot-me-database/Scripts/data.dml.sql
@@ -0,0 +1,4 @@
+DELETE FROM t_spots;
+
+INSERT INTO t_spots (spot_name, spot_lat, spot_lng, spot_desc) VALUES
+	('Le Social bar', 48.8418037, 2.3755198, 'Bar conviale' );
